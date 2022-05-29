94cba08e56faa710ffaca5112103f5ac3d610607
S Roze <sherozem4@gmail.com>
2022-05-29 21:36:46 +0100

    minor change to font

--- display-meme.js
+++ display-meme.js
@@ -1,17 +1,16 @@
 const ourUrl = window.location.search;
 let searchParams = new URLSearchParams(ourUrl);
 
-const topText = searchParams.get("top-text");
-const bottomText = searchParams.get("bottom-text");
-const url = searchParams.get("url");
+const topText = searchParams.get('top-text');
+const bottomText = searchParams.get('bottom-text');
+const url = searchParams.get('url');
 
-const topTextOnPage = document.getElementById("top-text");
-const bottomTextOnPage = document.getElementById("bottom-text");
-const imageUrl = document.querySelector("img");
-const memeArea = document.getElementById("meme");
+const topTextOnPage = document.getElementById('top-text');
+const bottomTextOnPage = document.getElementById('bottom-text');
+const imageUrl = document.querySelector('img');
+const memeArea = document.getElementById('meme');
 
 topTextOnPage.innerText = `${topText}`;
 bottomTextOnPage.innerText = `${bottomText}`;
 
 memeArea.style.backgroundImage = `url(${url})`;
-// imageUrl.src = url;
